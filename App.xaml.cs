fae5268138206fb6e6769481b9c9a1269723a16e
appsure <serge@appsure.co>
2023-09-12 22:17:01 -0400

    First

---
+++ App.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MrGesturesMaui8RC1;
+
+public partial class App : Application
+{
+	public App()
+	{
+        MR.Gestures.Border b = new MR.Gestures.Border();
+        b.BackgroundColor = Colors.Green;
+        b.Up += async (a, b) =>
+        {
+            ContentPage cp2 = null;
+            var b1 = new Button() { Text = "Back" };
+            b1.Clicked += async (a, b) =>
+            {
+                var np = MainPage as NavigationPage;
+                await np.Navigation.PopAsync();
+            };
+            cp2 = new ContentPage()
+            {
+                Content = b1
+            };
+
+            var np = MainPage as NavigationPage;
+            await np.Navigation.PushAsync(cp2, true);
+
+        };
+
+        var cp = new ContentPage()
+        {
+            Content = b
+        };
+
+
+
+        /*App.MainPage = new CodeMarkup.Maui.NavigationPage()
+        {
+            p
+        };*/
+        MainPage = new NavigationPage(cp);
+    }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using MR.Gestures;
+
+namespace MrGesturesMaui8RC1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .ConfigureMRGestures()//"DW5K-FZ2G-2VHD-7ZHY-DM5V-B22A-UK3S-EK7M-3CB9-PJ8H-L3GZ-QWMY-9TVJ")
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
+
